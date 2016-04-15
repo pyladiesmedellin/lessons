4a04d809eda9d2b1f7271ad0a45df5a2b0e7de77
jhonjairoroa87 <jhonjairoroa87@gmail.com>
2016-04-15 18:56:56 -0500

    - added first method workshop for messon 3  - added documentation to lesson4/2_1_lists_read

---
+++ lesson_3/workshop/method_1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Modificar el contenido del metodo the_flying_circus para the retorne el valor booleano True
+"""
+
+def the_flying_circus():
+    return True
+
+### Omitir el codigo bajo ésta linea
+nuevo_valor = the_flying_circus()
+print nuevo_valor # imprime el valore retornado por  el metodo the_flying_circus
--- lesson_4/2_1_lists_read.py
+++ lesson_4/2_1_lists_read.py
@@ -6,16 +6,22 @@ lista_de_asignaturas = ['fisica', 'biologia', 'matematicas', 'programacion']
 
 # imprime la primera asignatura
 print "la primera asignaturas es: ", lista_de_asignaturas[0]
+# la primera asignaturas es:  fisica
 
 # imprime la segunda asignatura
 print "la segunda asignaturas es: ", lista_de_asignaturas[1]
+# la segunda asignaturas es:  biologia
 
 # imprime la ultima asignatura
 print "la ultima asignaturas es: ", lista_de_asignaturas[-1]
+# la ultima asignaturas es:  programacion
 
 # imprime los elementos incluídos desde la posición 1(incluido 1) a la posición 2(excluyendo 2)
 print "los elementos incluídos desde la posición 1 a la posición 2: ", lista_de_asignaturas[1:2]
+# los elementos incluídos desde la posición 1 a la posición 2:  ['biologia']
 
 # imprime los elementos incluídos desde la posición 1(incluido 1) a la posición 3(excluyendo 3)
 print "los elementos incluídos desde la posición 1 a la posición 3: ", lista_de_asignaturas[1:3]
+# los elementos incluídos desde la posición 1 a la posición 3:  ['biologia', 'matematicas']
+
 
